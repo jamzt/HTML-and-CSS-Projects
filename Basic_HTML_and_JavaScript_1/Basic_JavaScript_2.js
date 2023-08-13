a46d90a1647211f8068768c5c999a9415679c15e
jamzt <jamietaalai@gmail.com>
2023-08-13 12:45:55 -0700

    basic html and javascript
    utilized <img alt> <b> <i> <u> size, face, color

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_2.js
@@ -0,0 +1,6 @@
+function My_First_Function() {
+    var String = "Today is August 11,2023!";
+    var result = String.fontcolor("purple");
+    document.getElementById("Today").innerHTML =
+    result;
+}
